cdab25ebf71e8285647c46c1175712ee2d88cf48
George Butiri <george.butiri@coinninja.com>
2019-06-05 11:25:05 -0400

    updates to the login / logout screen

--- www/js/index.js
+++ www/js/index.js
@@ -4,6 +4,36 @@ var app = {
     initialize: function() {
         $('#phone_number').inputmask({"mask": "(999) 999-9999"});
         $('#verify_code').inputmask({"mask": "999999"});
+        
+        $(document).on('click', '#btn_logout', function(e) {
+            e.preventDefault();
+            $.ajax({
+                url: 'https://iseestudios.com/services/logout',
+                type: 'post',
+                data: 'luser_id=' + window.localStorage.getItem('luser_id') + '&token=' + window.localStorage.getItem('luser_token'),
+                dataType: 'json',
+                success: function(d) {
+                    window.localStorage.setItem('luser_id', '');
+                    window.localStorage.setItem('luser_token', '');
+                    postAjax(d);
+                    remove_loading_circle();
+                },
+            });
+        });
+        
+        if (typeof(window.localStorage.getItem('luser_token')) !== 'undefined' && typeof(window.localStorage.getItem('luser_id')) !== 'undefined') {
+            show_loading_circle();
+            $.ajax({
+                url: 'https://iseestudios.com/services/handshake',
+                type: 'post',
+                data: 'luser_id=' + window.localStorage.getItem('luser_id') + '&token=' + window.localStorage.getItem('luser_token'),
+                dataType: 'json',
+                success: function(d) {
+                    postAjax(d);
+                    remove_loading_circle();
+                },
+            });
+        }
     },
 
     // deviceready Event Handler
